bc1a65a87eea6ae6ac021c90a0118f4137392a3a
limhyunjo <sibagyeoni@gmail.com>
2024-01-18 17:50:34 +0900

    20240118 JS 공부

---
+++ js/14_입력한색으로변경.js
@@ -0,0 +1,12 @@
+ /* 입력한 색상으로 배경색 변경하기 */
+
+ const box = document.querySelector("#box");
+ const input = document.querySelector("#input");
+ const btn1 = document.querySelector("#btn1");
+
+
+ function clsFn(){
+
+
+    
+ }
