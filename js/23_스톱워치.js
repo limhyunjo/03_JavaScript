b6b37b2f75f8f916a519c4352427bd9a5563be4e
limhyunjo <sibagyeoni@gmail.com>
2024-01-23 17:08:02 +0900

    2024-01-23 JS( window 내장 객체, 기본이벤트제거, 정규표현식

--- js/23_스톱워치.js
+++ js/23_스톱워치.js
@@ -19,6 +19,16 @@ let currInterval; //인터벌 저장 변수 선언 해놓음
 /* START 버튼 클릭 시 10ms 마다 display(시간)을 증가 */
 startBtn.addEventListener("click", e => {
 
+  //클릭한 버튼에 작성된 내용이 "PAUSE"인 경우/ 일시 정지 만들기
+   if(e.target.textContent == "PAUSE"){
+     e.target.textContent = "START";// 버튼 내용 START로 변경
+
+      window.clearInterval(currInterval);//currInterval 정지
+      return; 
+   }
+
+
+  // 클릭한 버튼에 작성된 내용이 "start"인 경우
   currInterval = window.setInterval( ()=> {
 
     count++;//count 1증가
@@ -28,8 +38,18 @@ startBtn.addEventListener("click", e => {
     // document.querySelector("#display>span")[2].innerText = count;
 
   }, 10);//10ms
-  e.target.disabled = true; //임시로 한번만 누를 수 있게 만들었음
-});
+  // e.target.disabled = true; //(임시) 한번만 누를 수 있게 하는 기능
+   e.target.textContent= "PAUSE";//textContent innerText 대신 요즘 많이 씀
+}); 
+
+/* 요소.textContent 와 요소.innerText */
+
+// textContent를 이용해 요소에 작성된 내용 얻어오기
+// -> HTML 파일에 작성된 내용 형태(단, 태그 빼고) 그대로 얻어옴
+//   (띄어쓰기, 엔터 모두 포함)
+
+//innerText를 이용해 요소에 작성된 내용 얻어오기
+//-> 브라우저 화면에 보이는 형태(단, 태그 빼고)를 얻어옴
 
 /* 시계 출력 함수 */
 function output(){
@@ -38,7 +58,7 @@ function output(){
     let second = Math.floor( count / 100 % 60 );
     let ms     = count % 100;
 
-   // 0 붙여서 문자열로 변환해서 반환
+   // 0 붙여서 두자리 문자열로 변환해서 반환
    minute = attachZero(minute);
    second = attachZero(second);
    ms     = attachZero(ms);
@@ -79,6 +99,37 @@ resetBtn.addEventListener("click", () => {
   output();
 
   //start 버튼 활성화( == 비활성화 안하겠다)
-  startBtn.disabled =false;
+  // startBtn.disabled =false;
 
+  //START 버튼의 내용을 "START"로 변경
+  startBtn.textContent ="START";
+
+  //#recordContainer의 내용을 모두 삭제 (== 빈 문자열로 변경)
+  recordContainer.innerHTML = "";
+});
+
+/* 
+  recordBtn 클릭 시
+
+  li 요소 생성
+  li 요소의 내용으로
+  #display에 출력된 시간을 얻어와
+  대입
+
+  #recordContainer의
+  첫 번째 자식으로 li 추가
+*/
+
+/* recordBtn 클릭 시 */
+recordBtn.addEventListener("click", () => {
+
+  // li 요소 생성
+  const li = document.createElement("li");
+
+  // li 요소의 내용으로
+  // #display에 출력된 시간을 얻어와 대입
+  li.innerText = display.innerText;
+
+  // #recordContainer의 첫 번째 자식으로 li 추가
+  recordContainer.prepend(li);
 });
